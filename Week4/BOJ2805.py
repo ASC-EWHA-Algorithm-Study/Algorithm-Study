046accd20e27638a766aceb4e06a96be4a637df3
pinktopaz <heather128@ewhain.net>
2023-11-07 12:02:23 +0900

    BOJ2805 복습

--- Week4/BOJ2805.py
+++ Week4/BOJ2805.py
@@ -1,17 +1,26 @@
-N, M = map(int, input().split())
-tree = list(map(int, input().split()))
-start, end = 1, max(tree) 
-
-while start <= end: 
-    mid = (start+end) // 2
+import sys
     
-    log = 0 
-    for i in tree:
-        if i >= mid:
-            log += i - mid
+N, M = map(int, sys.stdin.readline().split(" "))
+logs = list(map(int, sys.stdin.readline().split(" ")))
 
-    if log >= M:
-        start = mid + 1
-    else:
-        end = mid - 1
-print(end)
+def binary_search(arr, start, end) : 
+    ans = 0
+    while start <= end : 
+        mid = (start + end) // 2
+        total = 0 
+        
+        for x in arr : 
+            if x > mid :
+                total += x - mid 
+            
+        if total > M : 
+            ans = mid 
+            start = mid +1 
+        elif total == M : 
+            ans = mid 
+            break
+        else : 
+            end = mid - 1
+    return ans 
+
+print(binary_search(logs, 0, max(logs))) 
