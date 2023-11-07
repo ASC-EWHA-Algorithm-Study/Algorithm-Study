74432b4849cd2c199fec9931529715c79b61d2d0
pinktopaz <heather128@ewhain.net>
2023-11-07 17:55:31 +0900

    BOJ1417 복습

--- Week4/BOJ1417.py
+++ Week4/BOJ1417.py
@@ -1,24 +1,28 @@
-import heapq
+import sys
 
-n = int(input())
+# 가장 득표수가 많은 사람을 계속 확인해서 매수해서 
+# 다솜이의 득표수를 올리는 걸 반복하다가 
+# 제일 득표수가 많은 사람이 다솜이보다 득표수가 낮아지는 순간 
+# 매수를 멈추면 된다
 
-hq = []
-for i in range(n):
-    votes = int(input())
-    if i == 0:
-        dasom_votes = votes
-        continue
+N = int(sys.stdin.readline())
+dasom_votes = int(sys.stdin.readline())
+votes = []
+for i in range(N-1) : 
+    votes.append(int(sys.stdin.readline()))
+votes.sort(reverse=True)
 
-    heapq.heappush(hq, -votes)
+cnt = 0 
 
-bribe_count = 0
-while hq:
-    votes = -heapq.heappop(hq)
-    if votes < dasom_votes:
-        break
+if N == 1: 
+    print(0)
+else : 
+    while votes[0] >= dasom_votes : 
+        dasom_votes += 1
+        votes[0]-=1
+        cnt +=1  
+        votes.sort(reverse=True)  
+    print(cnt)
 
-    dasom_votes += 1
-    bribe_count += 1
-    heapq.heappush(hq, -(votes - 1))
-
-print(bribe_count)
+    
+    
