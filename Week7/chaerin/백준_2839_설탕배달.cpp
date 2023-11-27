61f3b9b907504d3431559a9da51521cdd0c0dd3c
chaerin <100216331+julia-heo@users.noreply.github.com>
2023-11-28 07:29:07 +0900

    Update 백준_2839_설탕배달.cpp

--- Week7/chaerin/백준_2839_설탕배달.cpp
+++ Week7/chaerin/백준_2839_설탕배달.cpp
@@ -8,7 +8,7 @@ int main()
     int N,ans=-1;;
     cin>>N;
     int th=N/3;
-    for(int i=0;i<th;i++){
+    for(int i=0;i<=th;i++){
         int temp=N-i*3;
         if(temp%5==0){
             ans=i+temp/5;
